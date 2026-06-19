00c0808564553ec4a203a2b7767be1f29ff403bc
James <theratdefence@gmail.com>
2026-06-19 17:07:23 +1000

    Add Screen self-registration decorator and registry

--- Objects/interfaces.py
+++ Objects/interfaces.py
@@ -10,6 +10,31 @@ class Screen(ABC):
     An interface that all screens must implement in order to be used in the main game loop.
     """
 
+    #TODO(): Document Registry Logic
+
+    # ----------| Registry |---------- #
+    _registry: dict[str, "type[Screen]"] = {}   # Have to resolve the Screen forward reference using a string literal
+
+    @staticmethod
+    def register_screen(screen_name: str):
+        """
+        A decorator that registers classes that are to be built by MainDisplay
+        :param screen_name: The name of the screen as a string (used when transitioning screens)
+        """
+        def decorator(subclass: type[Screen]):
+            Screen._registry[screen_name] = subclass
+            return subclass
+        return decorator
+
+    @staticmethod
+    def get_registry() -> dict[str, "type[Screen]"]:    # Another forward reference
+        """
+        Retrieves the registry of screens
+        :return: dict[str, type[Screen]]
+        """
+        return Screen._registry
+
+
    # ---------| Properties |--------- #
     @property
     @abstractmethod

9bdb71d5b1e4b4a3e2ccd70d67228a68e5210946
James <theratdefence@gmail.com>
2026-06-21 12:29:29 +1000

    Create database.py with default openings

---
+++ Objects/database.py
@@ -0,0 +1,7 @@
+_DEFAULT_OPENINGS = [
+    ("Italian Game",     "1. e4 e5 2. Nf3 Nc6 3. Bc4"),
+    ("Sicilian Defence", "1. e4 c5 2. Nf3 d6 3. d4 cxd4 4. Nxd4 Nf6 5. Nc3"),
+    ("London System",    "1. d4 d5 2. Bf4 Nf6 3. e3 e6 4. Nf3 Bd6 5. Bg3"),
+    ("King's Indian",    "1. d4 Nf6 2. c4 g6 3. Nc3 Bg7 4. e4 d6 5. Nf3"),
+    ("Queen's Gambit",   "1. d4 d5 2. c4 e6 3. Nc3 Nf6 4. Bg5 Be7 5. e3"),
+]
